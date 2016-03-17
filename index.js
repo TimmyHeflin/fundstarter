d3171273e5ef5eb5649969a494f35871be67f0f2
Ubuntu <ubuntu@ip-172-31-43-3.us-west-2.compute.internal>
2016-03-17 01:55:36 +0000

    This is for part 1a

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var express = require('express')
-var app = express()
+var fs = require('fs');
+var http = require('http');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var requestListener = function (req, res) {
+   res.writeHead(200);
+   res.write(fs.readFileSync(__dirname + '/index.html'));
+   res.end();
+}
 
-app.get('/', function(request, response) {
-response.sendfile('bitstarter.html') 
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var server = http.createServer(requestListener);
+server.listen(8080);
