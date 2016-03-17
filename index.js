208e4ed2cfd27b5cdac244c8a29653d247cdc692
Ubuntu <ubuntu@ip-172-31-43-3.us-west-2.compute.internal>
2016-03-17 04:33:46 +0000

    for Part 2

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var fs = require('fs');
 var http = require('http');
 var website;
+var buffer;
 
-fs.readFile(__dirname + '/index.html', function (err, data) {
+fs.open(__dirname + '/index.html', 'r', function(err, fd) {
     if(err) {
-	return console.log(err);
+        return console.log(err);
     }
-    website = data;
+
+    fs.stat(__dirname + '/index.html', function(err, stats) {
+        if(err) {
+	    return console.log(err);
+	}
+        
+	buffer = new Buffer(stats.size);
+
+        fs.read(fd, buffer, 0, buffer.length, null, 
+		function(err, bytesRead, buffer) {
+		    if(err) {
+			return console.log(err);
+		    }
+
+	            website = buffer.toString('utf8', 0, bytesRead); 
+		    
+	});
+    });
 });
 
 var requestListener = function (req, res) {
