40942a942c30e0bb1fe291377fbd8580ff0dd1a6
Ubuntu <ubuntu@ip-172-31-43-3.us-west-2.compute.internal>
2016-03-17 02:40:21 +0000

    This is for part 1 b

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var fs = require('fs');
 var http = require('http');
+var website;
+
+fs.readFile(__dirname + '/index.html', function (err, data) {
+    if(err) {
+	return console.log(err);
+    }
+    website = data;
+});
 
 var requestListener = function (req, res) {
    res.writeHead(200);
-   res.write(fs.readFileSync(__dirname + '/index.html'));
+   res.write(website);
    res.end();
 }
 
